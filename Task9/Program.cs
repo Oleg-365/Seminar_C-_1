270736e517f635ac03fc944e932453542bf7b822
PopovOleg <jim_power@mail.ru>
2023-04-28 13:57:36 +0300

    .\Task9\

---
+++ Task9/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+//Например:
+//78 -> 8
+//12-> 2
+//85 -> 8
+
+Main();
+
+void Main()
+{
+    int rand = new Random().Next(10, 100);
+    Console.WriteLine("Рамдомное число:  " + rand);
+    int a1 = FindFirstDigitNumber(rand); //первая цифра рандомного числа.
+    int b1 = FindSecondDigitNumber(rand); //последняя цифра рандомного числа.
+    FindLargesNumber(a1, b1);
+
+    int rand1 = new Random().Next(10, 100);
+    Console.WriteLine("Рамдомное число:  " + rand1);
+    int a2 = FindFirstDigitNumber(rand1); //первая цифра рандомного числа.
+    int b2 = FindSecondDigitNumber(rand1); //последняя цифра рандомного числа.
+    FindLargesNumber(a2, b2);
+
