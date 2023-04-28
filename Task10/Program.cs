b83a5919c545ef18450819b5c5c82e09a44c4dcf
PopovOleg <jim_power@mail.ru>
2023-04-28 14:01:35 +0300

    .\Task10\

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int rand = new Random().Next(100, 1000);
+
+Console.WriteLine("Рамдомное число:  " + rand);
+Console.WriteLine(" " + rand);
+
+FistNumber(rand);
+
+int FistNumber(int x)
+{
+    int a = x / 100;
+    int b = x % 10;
+    rand = (a * 10) + b;
+    return x;
+}
+
