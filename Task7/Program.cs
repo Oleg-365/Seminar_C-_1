127e562761bf27660a31a87147261feb02f9e282
PopovOleg <jim_power@mail.ru>
2023-04-28 13:48:39 +0300

    .\Task7\

---
+++ Task7/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача №6
+//Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+//Пример:
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Write("Enter a number: ");
+
+int number = int.Parse(Console.ReadLine()!);
+
+if (number%2 == 0)
+{
+    Console.Write("An even number and divisible by 2" );
+}
+else
+{
+    Console.Write("An odd number and not divisible by 2");
+}
