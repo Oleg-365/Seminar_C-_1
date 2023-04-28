b3f0e8eab89ca1ff33be98a21d51a4ae7476214b
PopovOleg <jim_power@mail.ru>
2023-04-28 15:24:40 +0300

    .\Task14\

---
+++ Task14/Program.cs
@@ -0,0 +1,44 @@
+﻿//Эталлонное решение задачи
+//Задача 3: Напишите программу, которая принимает на вход цифру
+// обозначающую день недели, и проверяет, 
+//6 - да
+//7 - да
+//1 - нет
+
+int Promt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine()!;
+    int result = Convert.ToInt32(value);
+    return result;
+}
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekday(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели!");
+    return false;
+}
+
+int weekDay = Promt("Введите день недели > ");
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("Наконец-то выходной!");
+    }
+    else
+    {
+        Console.WriteLine("Придется поработать");
+    }
+}
