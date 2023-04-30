2547add0904e707b7c8b8fa24fe415af394d0f12
popov_oleg <jim_power@mail.ru>
2023-04-30 17:44:49 +0300

    .\Task17\

---
+++ Task17/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int x1 = Promt("Введите координату Xa: ");
+int y1 = Promt("Введите координату Ya: ");
+int x2 = Promt("Введите координату Xb: ");
+int y2 = Promt("Введите координату Yb: ");
+
+double d = FindLength(x1, y1, x2, y2);
+
+double FindLength(int xa, int ya, int xb, int yb)
+{
+    double result = Convert.ToDouble(Math.Round(Math.Sqrt(((xb-xa)*(xb-xa))+((yb-ya)*(yb-ya))),3));
+    Console.Write(result);
+    return result;
+}
+
+int Promt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    int coord = Convert.ToInt32(value);
+    return coord;
+}
