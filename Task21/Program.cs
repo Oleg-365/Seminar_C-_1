d6e8846cff7058699fe66ad63f921cb18c0a0ac2
popov_oleg <jim_power@mail.ru>
2023-05-18 22:07:42 +0300

    .\Task21\

---
+++ Task21/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+
+int Promt()
+{
+    Console.Write("Введите число: ");
+    int limit = int.Parse(Console.ReadLine()!);
+    return limit;
+}
+int GetComp(int limit)
+{
+    int comp = 1;
+    for (int i=1; i <= limit; i++)
+    {
+        comp = comp * i;
+    }
+    return comp;
+}
+
+int num = Promt();
+Console.WriteLine($"Произведение чисел от 1 до {num} равно = {GetComp(num)}");
+
+
