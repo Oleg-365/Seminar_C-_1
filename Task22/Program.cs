222e4ede76f06ef9598d0371b5eeb41be00d9b2b
popov_oleg <jim_power@mail.ru>
2023-05-18 22:08:37 +0300

    .\Task22\

---
+++ Task22/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая
+//выводит массив из 8 элементов, заполненный
+//нулями и единицами в случайном порядке.
+
+
+
+void Initialized(int[] a){
+    for(int i=0; i<a.Length; i++)
+    {
+        a[i]=new Random().Next(0,2);
+    }
+}
+
+void Write(int[] a)
+{
+    for(int i=0; i<a.Length; i++)
+    {
+        Console.Write(a[i]+" ");
+    }
+}
+
+int[] a={1, 1, 1, 1, 1, 1, 1, 1};
+Initialized(a);
+Write(a);
