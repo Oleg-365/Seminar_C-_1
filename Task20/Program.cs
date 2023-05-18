0ac9858b191e1cdf780f165153e3d6f93042a8f4
popov_oleg <jim_power@mail.ru>
2023-05-18 22:07:22 +0300

    .\Task20\

---
+++ Task20/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+int Promt()
+{
+    Console.Write("Введите число: ");
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int Number(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count = count + 1;
+    }
+    return count;
+}
+
+int num = Promt();
+Console.WriteLine($"Количество цифр в числе {num} равно  {Number(num)}");
