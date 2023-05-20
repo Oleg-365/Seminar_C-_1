e62c8a9bede5def614e3b6e3adfe982ba600aa03
Oleg Popov <jim_power@mail.ru>
2023-05-20 19:02:14 +0300

    .\Task23\

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,51 +1,69 @@
-﻿//Задача 25: Напишите программу, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
-class program
+
+
+Main();
+
+void Main()
 {
-    static Main();
-   
-    void Main()
-    {
-        PromtA();
-        PromtB();
-        Degree (userNumA, userNumB);
-    //Console.WriteLine
-    }
- 
-    int PromtA()
-    {
-        Console.Write("Введите первое число: ");
-        int userNumA = int.Parse(Console.ReadLine()!);
-        return userNumA; 
-    }
-    int PromtB() 
-    {  
-    Console.Write("Введите второе число: ");
-    int userNumB = int.Parse(Console.ReadLine()!);
-    return userNumB;
-    }
-
-        double Degree (int userNumA, int userNumB)
-    {
-        double numer = userNumA;
-        int stepen = Convert.ToInt32(userNumB);
-    
-    //if (numer>0 && stepen>0)
-    //{
-       int temp = Convert.ToInt32 (Math.Pow(numer, stepen));
-        return temp;
-    }
-    
+    int numberA = PromtA();
+    int numberB = PromtB();
+    Degree(numberA, numberB);
 }
-    //else
-    //Console.WriteLine($"{numer}^{stepen} ="  + Math.Pow(numer, stepen));
-    //Console.WriteLine("5^4 = " + Math.Pow(5, 4));
-    //Console.WriteLine("-5^4 = " + Math.Pow(-5, 4));
-    //Console.WriteLine("-5^(-4) = " + Math.Pow(-5, -4));;
-    //return result;
- //}
-    
+int PromtA()
+{
+    Console.Write("Введите первое число: ");
+    int NumA = int.Parse(Console.ReadLine()!);
+    return NumA;
+}
+int PromtB()
+{
+    Console.Write("Введите второе число: ");
+    int NumB = int.Parse(Console.ReadLine()!);
+    return NumB;
+}
 
- 
+void Degree(int NumA, int NumB)
+{
+    int number = NumA;
+    int stepen = NumB;
+    int temp = 1;
+
+    if (stepen < 1)
+    {
+        Console.WriteLine($"Число {stepen} должно быть натуральным");
+    }
+    else
+    {
+        //int step = 1;
+        //for (int i = 1; 1 <= stepen; i++)
+        //{
+        //  step = step * number;
+        //}
+
+        while (stepen != 0)
+        {
+            temp = temp * number;
+            stepen = stepen - 1;
+        }
+        Console.Write($"Число {number} в натуральной степени {NumB} равно {temp}");
+    }
+
+    //return step;
+}
+
+//temp = Convert.ToInt32 (Math.Pow(numer, stepen));
+
+//Console.WriteLine($"{numer}^{stepen} =" + Math.Pow(numer, stepen));
+//else
+//Console.WriteLine($"{numer}^{stepen} ="  + Math.Pow(numer, stepen));
+//Console.WriteLine("5^4 = " + Math.Pow(5, 4));
+//Console.WriteLine("-5^4 = " + Math.Pow(-5, 4));
+//Console.WriteLine("-5^(-4) = " + Math.Pow(-5, -4));;
+//return result;
+//}
+
+
+
