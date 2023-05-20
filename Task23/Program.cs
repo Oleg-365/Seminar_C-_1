074fe74a8ef56f68bd66abd10bea74217e40a827
Oleg Popov <jim_power@mail.ru>
2023-05-20 22:56:52 +0300

    .\Task23 решил на половину задачу"

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -37,12 +37,6 @@ void Degree(int NumA, int NumB)
     }
     else
     {
-        //int step = 1;
-        //for (int i = 1; 1 <= stepen; i++)
-        //{
-        //  step = step * number;
-        //}
-
         while (stepen != 0)
         {
             temp = temp * number;
@@ -50,20 +44,4 @@ void Degree(int NumA, int NumB)
         }
         Console.Write($"Число {number} в натуральной степени {NumB} равно {temp}");
     }
-
-    //return step;
 }
-
-//temp = Convert.ToInt32 (Math.Pow(numer, stepen));
-
-//Console.WriteLine($"{numer}^{stepen} =" + Math.Pow(numer, stepen));
-//else
-//Console.WriteLine($"{numer}^{stepen} ="  + Math.Pow(numer, stepen));
-//Console.WriteLine("5^4 = " + Math.Pow(5, 4));
-//Console.WriteLine("-5^4 = " + Math.Pow(-5, 4));
-//Console.WriteLine("-5^(-4) = " + Math.Pow(-5, -4));;
-//return result;
-//}
-
-
-
--- Task24/Program.cs
+++ Task24/Program.cs
@@ -2,3 +2,36 @@
 //452 -> 11
 //82 -> 10
 //9012 -> 12
+
+
+Main();
+
+void Main()
+{
+    int number = Promt();
+    Validate(number);
+    Sum(number);
+}
+
+int Promt()
+{
+    Console.Write("Введите от 11 до 9999 число: ");
+    int Num = int.Parse(Console.ReadLine()!);
+    return Num;
+}
+
+int Validate(int Num)
+{
+    if (Num >= 11 || Num <= 9999)
+    {
+        return Num;
+    }
+    else
+    {
+        Console.Write("Вы ввели неверное число");
+    }
+}
+int Summa(int Num)
+{
+
+}
