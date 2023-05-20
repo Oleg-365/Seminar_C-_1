2efba96466f69ff904f5608cf482309e2001253e
popov_oleg <jim_power@mail.ru>
2023-05-20 08:41:03 +0300

    .\Task23\

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -2,38 +2,50 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
-Main();
-
-
-int Promt()
+class program
 {
-    Console.Write("Введите первое число: ");
-    Console.Write("Введите второе число: ");
-    int userNumA = int.Parse(Console.ReadLine()!);
-    int userNumB = int.Parse(Console.ReadLine()!);
-    return userNumA; userNumB;
-}
-//int PromtB()
-//{
-   // Console.Write("Введите второе число: ");
-   // int userNumB = int.Parse(Console.ReadLine()!);
-    //return userNumB;
-//}
-
- double degree (int userNumA, int userNumB)
- {
-    double numer = userNumA;
-    int stepen = Convert.ToInt32(userNumB);
-    //if (numer>0 && stepen>0)
+    static Main();
+   
+    void Main()
     {
-        Console.WriteLine($"{numer}^{stepen} ="  + Math.Pow(numer, stepen));
+        PromtA();
+        PromtB();
+        Degree (userNumA, userNumB);
+    //Console.WriteLine
     }
+ 
+    int PromtA()
+    {
+        Console.Write("Введите первое число: ");
+        int userNumA = int.Parse(Console.ReadLine()!);
+        return userNumA; 
+    }
+    int PromtB() 
+    {  
+    Console.Write("Введите второе число: ");
+    int userNumB = int.Parse(Console.ReadLine()!);
+    return userNumB;
+    }
+
+        double Degree (int userNumA, int userNumB)
+    {
+        double numer = userNumA;
+        int stepen = Convert.ToInt32(userNumB);
+    
+    //if (numer>0 && stepen>0)
+    //{
+       int temp = Convert.ToInt32 (Math.Pow(numer, stepen));
+        return temp;
+    }
+    
+}
     //else
+    //Console.WriteLine($"{numer}^{stepen} ="  + Math.Pow(numer, stepen));
     //Console.WriteLine("5^4 = " + Math.Pow(5, 4));
     //Console.WriteLine("-5^4 = " + Math.Pow(-5, 4));
     //Console.WriteLine("-5^(-4) = " + Math.Pow(-5, -4));;
     //return result;
- }
+ //}
     
 
  
