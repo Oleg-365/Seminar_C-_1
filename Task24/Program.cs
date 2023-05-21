716012d3efcb557711915c67a07cf94d35f16991
Oleg Popov <jim_power@mail.ru>
2023-05-21 23:00:03 +0300

    .\Task25 Добавил решение задачи"

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -11,8 +11,8 @@ void Main()
     int number = Promt();
     int len = NumberLen(number);
     Validate(number);
-    Sum (number, len);
-    Console.Write($"Сумма цифр числа {number} составляет {Sum (number, len)}.");
+    Sum(number, len);
+    Console.Write($"Сумма цифр числа {number} составляет {Sum(number, len)}.");
 }
 
 int Promt()
@@ -35,62 +35,62 @@ int Validate(int Num)
     return Num;
 }
 
-int NumberLen (int Num)
+int NumberLen(int Num)
 {
     int index = 0;
-    while (Num>0)
-{
-    Num/=10;
-    index++;
-}
+    while (Num > 0)
+    {
+        Num /= 10;
+        index++;
+    }
     return index;
 }
 
-int Sum (int Num, int length)
+int Sum(int Num, int length)
 {
-   int Summa = 0;
+    int Summa = 0;
     for (int index = 0; index < length; index++)
     {
-    Summa += Num%10;
-    Num/=10;
+        Summa += Num % 10;
+        Num /= 10;
     }
     return Summa;
 }
 
 //int Summa(int Num)
 //{
-    //int Sum = 0;
-   // int Summa1 = 0;
-   // int Summa2 = 0;
-   // int Summa3 = 0;
-   // Sum = Summa1 + Summa2 + Summa3;
+//int Sum = 0;
+// int Summa1 = 0;
+// int Summa2 = 0;
+// int Summa3 = 0;
+// Sum = Summa1 + Summa2 + Summa3;
 
-   // if (Num >= 11 || Num < 99)
-   // {
-   //     int tempC = Num / 10;
-  //      int tempD = Num % 1;
-   //     int Sum1 = 0;
-  //      Sum1 = tempC + tempD;
-   //     return Sum1;
-   // }
-  //  else if (Num >= 100 || Num < 999)
-   // {
-   //     int tempB = Num / 100;
-   //     int tempC = Num / 10;
-  //      int tempD = Num / 1;
-   //     int Sum2 = 0;
-  //      Sum2 = tempB + tempC + tempD;
-   //     return Sum2;
-   // }
-   // else if (Num >= 1000 || Num < 9999)
-   // {
-   //     int tempA = Num / 1000;
-  //      int tempB = Num / 100;
-   //     int tempC = Num / 10;
-   //     int tempD = Num / 1;
-   //     int Sum3 = 0;
-   //     Sum3 = tempA + tempB + tempC + tempD;
-   //     return Sum3;
-   // }
+// if (Num >= 11 || Num < 99)
+// {
+//     int tempC = Num / 10;
+//      int tempD = Num % 1;
+//     int Sum1 = 0;
+//      Sum1 = tempC + tempD;
+//     return Sum1;
+// }
+//  else if (Num >= 100 || Num < 999)
+// {
+//     int tempB = Num / 100;
+//     int tempC = Num / 10;
+//      int tempD = Num / 1;
+//     int Sum2 = 0;
+//      Sum2 = tempB + tempC + tempD;
+//     return Sum2;
+// }
+// else if (Num >= 1000 || Num < 9999)
+// {
+//     int tempA = Num / 1000;
+//      int tempB = Num / 100;
+//     int tempC = Num / 10;
+//     int tempD = Num / 1;
+//     int Sum3 = 0;
+//     Sum3 = tempA + tempB + tempC + tempD;
+//     return Sum3;
+// }
 //return Sum;
 //}
--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,3 +1,34 @@
 ﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 //1, 2, 5, 7, 19, 4, 76, 3 -> [1, 2, 5, 7, 19,4,76,3]
 //6, 1, 33, 43, 12, 3 ,2, 9 -> [6, 1, 33,43,12,3,2,9]
+
+Main();
+
+void Main()
+{
+    //const int[] lengthArr = 8; 
+    const int[] lengthArr = 8;
+    RandArray(8);
+    PrintArr(lengthArr);
+    
+}
+
+int[] RandArray(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+   
+void PrintArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" [{array[i]}] ");
+    }
+    Console.WriteLine();
+    
+}
