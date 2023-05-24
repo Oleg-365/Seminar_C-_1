dfb108efe5ce47e8c4c8d86d235757dfc962a6e5
PopovOleg <jim_power@mail.ru>
2023-05-24 13:04:10 +0300

    .\Task28.1 добавил решение задачи"

---
+++ Task28.1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+
+void CheckNumArr(int[] arr, int userNumMin, int userNumMax)
+{
+    int count = 0;
+    foreach (int el in arr)
+    {
+
+        if (el >= userNumMin && el <= userNumMax)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Количество числе в диапазоне от {userNumMin} до {userNumMax} {count}");
+}
+
+int[] array = GetArray(123, 0, 150);
+Console.WriteLine();
+CheckNumArr(array, 10, 99);
