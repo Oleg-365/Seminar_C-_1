087b7f2213bb1d2284981154d20a703cec4ef548
PopovOleg <jim_power@mail.ru>
2023-05-24 16:32:00 +0300

    .\Lesson4.1\

---
+++ Lesson4.1/Program.cs
@@ -0,0 +1,31 @@
+﻿/* int[,] pic = new int[,]
+// в записе записанной выше дан массив в лекции это знак "класс"
+
+void PrintImage(int[,] image) // метод вывода массива
+{
+    for (int i = 0; i < matr.GetLength(0); i++) //matrix.GetLength(0) где 0 это количество строк в даннм случае 3.
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)  ////matrix.GetLength(1) где 1 это количество строк в даннм случае 4.
+        {
+            // Console.Write($"{matr[i, j]} ");
+            if (image[i, j] == 0) Console.WriteLine($" ");
+            else Console.WriteLine($"+");
+        }
+        Console.WriteLine();
+    }
+}
+void FillImage(int row, int col) // метод закрашивания
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row - 1, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row - 1, col + 1);
+    }
+}
+
+PrintImage(pic);
+FillImage(13, 13);
+PrintImage(pic); */
--- Task12/Program.cs
+++ Task12/Program.cs
@@ -9,7 +9,7 @@
 int Promt(string message)
 {
     System.Console.Write(message);
-    string value = Console.ReadLine();
+    string value = Console.ReadLine()!;
     int result = Convert.ToInt32(value);
     return result;
 }
