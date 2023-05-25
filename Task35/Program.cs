f0c87a3e72cce37e3b26bf8b570e341bc3ef08e4
popov_oleg <jim_power@mail.ru>
2023-05-25 22:58:56 +0300

    .\Task35\

---
+++ Task35/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 42: Напишите программу, которая будет
+//преобразовывать десятичное число в двоичное.
+//45 -> 101101
+//3 -> 11
+//2 -> 10
+
+string ToBinary(int number)
+{
+    string binary = String.Empty;
+    while (number > 0)
+    {
+        binary = (number % 2).ToString() + binary;
+        number = number / 2;
+    }
+
+    return binary;
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{number} -> {ToBinary(number)}");
