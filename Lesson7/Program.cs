3cda464f11d7a4ed5d79c0039b654589881e6aa1
Oleg Popov <jim_power@mail.ru>
2023-05-30 23:19:13 +0300

    Lesson7

---
+++ Lesson7/Program.cs
@@ -0,0 +1,25 @@
+﻿//Рекурсия
+/*int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+FindWords("аисв", new char[3]); // char[3], char[4] и тд.*/
+
+//Игра пирамидка
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >>{on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+Towers();
