9a766e789b17a45d727f15290b6978f9ce111109
PopovOleg <jim_power@mail.ru>
2023-06-01 12:11:19 +0300

    Lesson4.2

---
+++ Lesson4.2/Program.cs
@@ -0,0 +1,31 @@
+﻿//Метод Рекурсии 
+//Задача на вычисление факториала
+
+//При использовании типа данных int происходит переполнение и факториал на выходе 
+// получается отрицательным
+
+//int Factorial(int n)
+//{
+    //1! =1
+    //0! =1
+
+//    if (n == 1) return 1;
+//    else return n * Factorial(n - 1);
+//}
+//for (int i = 1; i < 40; i++)
+//{
+//    Console.WriteLine($"{i}! = {Factorial(i)}"); 
+//}
+
+double Factorial(int n)
+{
+    //1! =1
+    //0! =1
+
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}"); 
+}
