a424c3c75a8fa712d4b73415a5f7025f8c26afa8
popov_oleg <jim_power@mail.ru>
2023-06-07 07:35:13 +0300

    .\Task57\

---
+++ Task54/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 63: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"*/
+
+Console.Clear();
+Console.Write("Enter a number: ");
+
+int number = int.Parse(Console.ReadLine()!);
+int start = 1;
+
+Console.Write($" ");
+SizeNumber(number, start);
+
+void SizeNumber(int number, int start)
+{
+    if (start <= number)
+    {
+        Console.Write($"{start}  ");
+        SizeNumber(number, start + 1);
+    }
+}
+
+
+
---
+++ Task57/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8*/
+
+int a = Prompt("Введите число A: "); 
+int n = Prompt("Введите число N: "); 
+Console.Write(PowerRecV2(a, n)); 
+ 
+int Prompt(string message) 
+{ 
+    Console.Write(message); 
+    int value = Convert.ToInt32(Console.ReadLine()); 
+    return value; 
+} 
+ 
+int PowerRecV2(int numA, int numN) 
+{ 
+    if (numN == 0) return 1; 
+    else return PowerRecV2(numA, numN - 1) * numA; 
+}
